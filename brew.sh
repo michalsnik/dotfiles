828e3ff8033a6f3b4ba3e215e322a183fd5264b9
Michał Sajnóg <michal.sajnog@hotmail.com>
2017-01-25 22:50:46 +0100

    update brew list

--- brew.sh
+++ brew.sh
@@ -8,17 +8,25 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-brew cask install google-chrome
-brew cask install firefox
-brew cask install spotify
-brew cask install slack
 brew cask install hyper
 brew cask install iterm2
-brew cask install dash
-brew cask install postman
-brew cask install skype
 brew cask install postgres
-brew cask install utorrent
+brew cask install postman
 brew cask install visual-studio-code
+brew cask install github-desktop
+
+brew cask install dash
+brew cask install clipmenu
+
+brew cask install slack
+brew cask install skype
 brew cask install nylas-mail
+brew cask install dropbox
+
+brew cask install google-chrome
+brew cask install firefox
+
+brew cask install spotify
+brew cask install utorrent
 brew cask install boom
+brew cask install vlc
